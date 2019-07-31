f92be0264052001b9ba3c5596ea3486bda4a8e4a
petarsiderov <petar.siderov@avicenna4it.com>
2019-07-31 13:02:50 +0200

    added Contact.php content and added resource.php content

--- contact.php
+++ contact.php
@@ -1,4 +1,41 @@
 <?php require'layouts/header.php' ?>
+<section id="SourceImg" class="feature-image feature-image-default-alt" data-type="background" data-speed="5">
+        <div class="container">
+            <h1 id="SourceH1" class="page-title text-center">Contact</h1>
+        </div>
+    </section>
 
+
+    <section>
+       
+    <div class="container">
+        <h3 class="text-center">Have any question about the course? Feel free to get in touch with me? I'll do my best to get back to you ASAP. </h3>
+        <br>
+        <br>
+        <form>
+            <div class="form-row">
+                <div class="col-md-7">
+                <input type="text" class="form-control" placeholder="First name">
+            </div>
+                <div class="col-md-5">
+                <input type="text" class="form-control" placeholder="Last name">
+                </div>
+            </div>
+        </form>
+        <br>
+        <br>
+                <div class="form-group">
+                <div id="message"class="form-group col-md-12">
+                    <input type="text" class="form-control" id="inputText" placeholder="your message">
+                </div>
+                    
+                </div>
+                <div class="form-group col-md-12">
+                    <button type="submit" class="btn btn-primary pull-right">Sign in</button>
+                </div>
+            </form>
+
+</div>
+    <section>
 
 <?php require'layouts/signup.php'; require'layouts/footer.php' ?>
--- resources.php
+++ resources.php
@@ -5,4 +5,76 @@
             <h1 id="ResourceH1" class="page-title text-center">Resource</h1>
         </div>
     </section>
+
+       <!--BOOST YOUR INCOME -->
+       <section id="boost-income">
+        <div class="container">
+            <p class="resurce-p"> 
+                Welcome to the Bootstrap to WordPress resource page! On this page, I will provide you with usefull resources,
+                plugins, tools, applications and websites to help make your life as a coder a little bit easier. 
+                <b>I recommend adding this page to your bookmarks for convenient future reference.</b>
+                <br>
+                <br>
+                <small>
+                <i>
+                    Please keep in mind some of the following resources aare amy affiliate links, and should you decide
+                    to make a pruchase, I will earn a small commission -at no extra cost to you!.
+                    Please keep in mind some of the following resources aare amy affiliate links, and should you decide
+                    to make a pruchase, I will earn a small commission -at no extra cost to you!.
+                </i>
+                </small>
+            </p>
+            <div class="resource-row clearfix">
+                <div class="resource">
+                    <div class="row">
+                    <div class="col-sm-3 ">
+                    <img src="assets/img/justhost-logo.jpg" alt="justhost-logo">
+                    <h3><a href="www.justhost.com">Justhost</a></h3> 
+                    <p> 
+                        When I made a web site very first time about 7 years ext 
+                        When I made a web site very first time about 7 years ext 
+                        When I made a web site very first time about 7 years ext 
+                        When I made a web site very first time about 7 years ext 
+                        When I made a web site very first time about 7 years ext 
+                        When I made a web site very first time about 7 years ext 
+
+                    </p>
+                    <button type="button" class="btn btn-success">Get started with Justhost</button>
+                    </div>
+                    <div class="col-sm-3 col-sm-offset-1">
+                    <img src="assets/img/coda2-logo.jpg" alt="coda2-logo">
+                    <h3><a href="www.coda2.com">Coda2</a></h3> 
+                    <p> 
+                        When I made a web site very first time about 7 years ext 
+                        When I made a web site very first time about 7 years ext 
+                        When I made a web site very first time about 7 years ext 
+                        When I made a web site very first time about 7 years ext 
+                        When I made a web site very first time about 7 years ext 
+                        When I made a web site very first time about 7 years ext 
+
+                    </p>
+                    <button type="button" class="btn btn-success">Check out coda 2</button>
+                    </div>
+                    <div class="col-sm-3 col-sm-offset-2">
+                    <img src="assets/img/dropbox-logo.jpg" alt="dropbox-logo">
+                    <h3><a href="www.dropbox.com">Dropbox</a></h3> 
+                    <p> 
+                        When I made a web site very first time about 7 years ext 
+                        When I made a web site very first time about 7 years ext 
+                        When I made a web site very first time about 7 years ext 
+                        When I made a web site very first time about 7 years ext 
+                        When I made a web site very first time about 7 years ext 
+                        When I made a web site very first time about 7 years ext 
+
+                    </p>
+                    <button type="button" class="btn btn-success">Try dropbox for free</button>
+                    </div>
+                </div>
+                   
+                    
+                </div>
+            </div>
+        </div>
+        <!--Container-->
+    </section>
  <?php require'layouts/signup.php'; require'layouts/footer.php'?>
